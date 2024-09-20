515b70aeaa891ea802168d6d3be45e398c5d7c41
Omar Hernandez <omrhvs@gmail.com>
2024-09-20 02:14:47 -0600

    First commit

---
+++ src/paqueteinicial/VentanaInicial.java
@@ -0,0 +1,137 @@
+package paqueteinicial;
+
+import javax.swing.JOptionPane;
+
+public class VentanaInicial extends javax.swing.JFrame
+{
+
+    public VentanaInicial() 
+    {
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        barraHerramientas = new javax.swing.JToolBar();
+        btnUno = new javax.swing.JButton();
+        btnDos = new javax.swing.JButton();
+        btnTres = new javax.swing.JButton();
+        btnCuatro = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        barraHerramientas.setRollover(true);
+
+        btnUno.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/save.png"))); // NOI18N
+        btnUno.setFocusable(false);
+        btnUno.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnUno.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnUno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUnoActionPerformed(evt);
+            }
+        });
+        barraHerramientas.add(btnUno);
+
+        btnDos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/print.png"))); // NOI18N
+        btnDos.setFocusable(false);
+        btnDos.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnDos.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnDos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDosActionPerformed(evt);
+            }
+        });
+        barraHerramientas.add(btnDos);
+
+        btnTres.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/reload.png"))); // NOI18N
+        btnTres.setFocusable(false);
+        btnTres.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnTres.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnTres.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTresActionPerformed(evt);
+            }
+        });
+        barraHerramientas.add(btnTres);
+
+        btnCuatro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/leave.png"))); // NOI18N
+        btnCuatro.setFocusable(false);
+        btnCuatro.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnCuatro.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnCuatro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCuatroActionPerformed(evt);
+            }
+        });
+        barraHerramientas.add(btnCuatro);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(barraHerramientas, javax.swing.GroupLayout.PREFERRED_SIZE, 408, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(barraHerramientas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 240, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnUnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUnoActionPerformed
+        JOptionPane.showMessageDialog(null, "Haz pulsado el botón Uno.");
+    }//GEN-LAST:event_btnUnoActionPerformed
+
+    private void btnDosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDosActionPerformed
+        JOptionPane.showMessageDialog(null, "Haz pulsado el botón Dos.");
+    }//GEN-LAST:event_btnDosActionPerformed
+
+    private void btnTresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTresActionPerformed
+        JOptionPane.showMessageDialog(null, "Haz pulsado el botón Tres.");
+    }//GEN-LAST:event_btnTresActionPerformed
+
+    private void btnCuatroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCuatroActionPerformed
+        JOptionPane.showMessageDialog(null, "Haz pulsado el botón Cuatro.");
+    }//GEN-LAST:event_btnCuatroActionPerformed
+
+    public static void main(String args[]) 
+    {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                new VentanaInicial().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToolBar barraHerramientas;
+    private javax.swing.JButton btnCuatro;
+    private javax.swing.JButton btnDos;
+    private javax.swing.JButton btnTres;
+    private javax.swing.JButton btnUno;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    // End of variables declaration//GEN-END:variables
+}
